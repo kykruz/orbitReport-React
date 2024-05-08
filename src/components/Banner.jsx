26af8cf0915e342d25309f53522a1d687134c7b9
kykruz <k.kruzan@yahoo.com>
2024-05-08 09:45:09 -0500

    GA 6 After Introduction

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,7 @@ import "./styling.css"
 export default function Banner() {
     return (
         <div>
-            <header>
-                <h1>Orbit Report</h1>
-            </header>
+            <header>Orbit Report</header>
             <p>Click on the buttons to see the satellites in that orbit type</p>
         </div>
     )
