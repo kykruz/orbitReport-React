848d29193d5b7fb2a408db5b20ede6457b992e7e
kykruz <k.kruzan@yahoo.com>
2024-05-08 10:53:25 -0500

    GA 6 after demo

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,25 @@ import Table from "./components/Table";
 import satData from "./components/satData"
 
 function App() {
-  
+
   const [sat, setSat] = useState(satData);
   const displaySats = [...new Set(satData.map((data) => data.orbitType))];
 
   const filterByType = (currentType) => {
     const displaySats = satData.filter((newSatDisplay) => {
-       return newSatDisplay.orbitType === currentType;
+      return newSatDisplay.orbitType === currentType;
     });
     setSat(displaySats);
- };
-  
-    return (
+  };
+
+  return (
     <div>
       <Banner />
       <Buttons
-       filterByType={filterByType}
-       setSat={setSat}
-       displaySats={displaySats}
-       />
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
       <Table sat={sat} />
     </div>
   );
